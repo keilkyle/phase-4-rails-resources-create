68f93d042351141c6c5c848fcb1eb4d9433cf2ca
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-06-27 17:51:44 -0700

    commit tree 2072c1b41900861334584ac47fab4fcaaad7b839

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,9 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    return json: bird, status: :created
+  end
+
 end
